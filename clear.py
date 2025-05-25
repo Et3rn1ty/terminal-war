0e3147a1403275eb032425a7617dc8739577a9f2
Doug <doug@dougs.mac.mini.lan>
2025-05-25 10:25:22 -0400

    forgot to get rid of the old implementation of screenclear

--- clear.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def clear():
-    # Clear console based on the operating system
-    if os.name == 'nt':  # For Windows
-        os.system('cls')
-    else:  # For Unix/Linux/Mac
-        os.system('clear')
--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 from player import Player
 from playing_cards import Deck
 import terminal_ui
-from clear import clear
 
 class Game:
     def __init__(self, player1_name, player2_name):
